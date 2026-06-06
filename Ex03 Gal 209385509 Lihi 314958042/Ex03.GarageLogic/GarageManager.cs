c96ca59ee9a28ebeda0dbe27263fbe0a6623c022
JusticeIL <galru3@mta.ac.il>
2026-06-06 21:28:36 +0300

    - Fixed the message printed while trying to charge with electricity a fuel based vehicle

--- Ex03 Gal 209385509 Lihi 314958042/Ex03.GarageLogic/GarageManager.cs
+++ Ex03 Gal 209385509 Lihi 314958042/Ex03.GarageLogic/GarageManager.cs
@@ -122,7 +122,7 @@ namespace Ex03.GarageLogic
             
             if(vehicleBattery == null)
             {
-                throw new FormatException($"The vehicle with license plate '{i_LicensePlate}' is not fuel based and therefore cannot be filled with fuel.");
+                throw new FormatException($"The vehicle with license plate '{i_LicensePlate}' is not battery based and therefore cannot be filled with electricity.");
             }
 
             vehicleBattery.addHoursToBatteryCapacityIfPossible(hoursToLoadBattery);
